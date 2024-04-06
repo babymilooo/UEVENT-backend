f6a0a165a2d4408c4060ee896b2e55f7154b2ca2
DarunkaKa <shmetterling.dk@gmail.com>
2024-04-06 12:09:15 +0300

    create spotify login register

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -9,8 +9,10 @@ import {
 } from "../tokens/tokenService";
 import { ILoginDto, IRegisterDto } from "../types/auth";
 import { createUser, findUserByEmail } from "../user/userService";
+import { spotifyApi } from "../config/spotifyConfig";
 
 const authRouter = express.Router();
+
 
 authRouter.post("/login", async (req, res) => {
   try {
@@ -20,6 +22,9 @@ authRouter.post("/login", async (req, res) => {
     const user = await findUserByEmail(loginInfo.email);
 
     if (!user) return res.status(404).json(errorMessageObj("User not found"));
+
+    if (typeof user.passwordHash !== "string" || typeof loginInfo.password !== "string")
+      return res.status(400).json(errorMessageObj("Email and password are required"));
 
     if (!bcrypt.compareSync(loginInfo.password, user.passwordHash))
       return res.status(403).json(errorMessageObj("Invalid password"));
@@ -40,6 +45,67 @@ authRouter.post("/login", async (req, res) => {
     return res.status(400).json(errorMessageObj("Invalid data"));
   }
 });
+
+
+
+authRouter.get("/spotify-auth", (req, res) => {
+  const scopes = ["user-read-private", "user-read-email"];
+  const state = typeof req.query.state === 'string' ? req.query.state : '';
+  res.send(spotifyApi.createAuthorizeURL(scopes, state));
+});
+
+authRouter.get("/callback", async (req, res) => {
+  try {
+    console.log("1111111");
+    const code = typeof req.query.code === "string" ? req.query.code : null;
+    const state = typeof req.query.state === "string" ? req.query.state : null;
+    console.log(code);
+    if (!code) return res.status(400).json(errorMessageObj("Invalid request"));
+
+    const data: any = await spotifyApi.authorizationCodeGrant(code);
+    console.log(data);
+    const refreshToken = data.body.refresh_token;
+    console.log(refreshToken);
+    spotifyApi.setAccessToken(data.body.access_token);
+    spotifyApi.setRefreshToken(refreshToken);
+
+    const me = await spotifyApi.getMe();
+    const { email, id } = me.body;
+
+    let user = await findUserByEmail(email);
+
+    if (!user) {
+      console.log("===========");
+      user = await createUser({ email, spotifyId: id, spotifyRefreshToken: refreshToken, isRegisteredViaSpotify: true });
+    } else {
+      const userRefreshToken = user.spotifyRefreshToken;
+      if (userRefreshToken) {
+        spotifyApi.setRefreshToken(userRefreshToken);
+        const refreshedData: any = await spotifyApi.refreshAccessToken();
+        const refreshedAccessToken = refreshedData.body.access_token;
+        spotifyApi.setAccessToken(refreshedAccessToken);
+      }
+    }
+
+    const tokenPayload = {
+      id: user._id,
+      timestamp: new Date().toISOString(),
+    };
+
+    const tokens = await signAuthTokens(tokenPayload);
+    await setAuthTokensToCookies(res, tokens);
+    const returnObj: any = user.toObject();
+    delete returnObj.passwordHash || '';
+    return res.status(200).json(returnObj);
+
+  } catch (error) {
+    console.error("Callback processing error:", error);
+    return res.status(400).json(errorMessageObj("Invalid data"));
+  }
+});
+
+
+
 
 authRouter.post("/logout", authGuard, async (req, res) => {
   await deleteAuthTokensFromCookies(res);
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,10 @@
 export interface IUserDto {
-  userName: string;
+  userName?: string;
+  spotifyId?: string;
   email: string;
-  password: string;
+  password?: string;
+  spotifyRefreshToken?: string;
+  isRegisteredViaSpotify?: boolean;
 }
 
 export interface IUserUpdateDto {
